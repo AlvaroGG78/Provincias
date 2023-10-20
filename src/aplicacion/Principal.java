465b764d8ac28da196dcb92158e3e7d7f8ce9185
Alvaro Garcia <alvaro.garciagonzalez@usp.ceu.es>
2023-10-20 17:54:22 +0200

    Actualización v3

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -23,24 +23,4 @@ public class Principal {
         System.out.println(leonProvincia);
 
     }
-
-    /*public static void main(String[] args) {
-        public void balboaMunicipio(String[] args) {
-            Localidad cantejeiraLocalidad = new Localidad("Cantejeira", 36);
-            Localidad lamagrandeLocalidad = new Localidad("Lamagrande", 14);
-            System.out.println(cantejeiraLocalidad.toString());
-            System.out.println(lamagrandeLocalidad.toString());    
-            Municipio balboaMunicipio = new Municipio("Balboa", cantejeiraLocalidad,lamagrandeLocalidad);
-            System.out.println(balboaMunicipio.toString());
-        }
-
-        public static void (String[] args) {
-            Localidad bonellaLocalidad = new Localidad("Bonella", 23);
-            Localidad castilloLocalidad = new Localidad("El Castillo", 16);
-            System.out.println(bonellaLocalidad.toString());
-            System.out.println(castilloLocalidad.toString()); 
-            Municipio rielloMunicipio = new Municipio("Riello", bonellaLocalidad,castilloLocalidad);
-            System.out.println(rielloMunicipio.toString());
-        }
-    }*/
 }
